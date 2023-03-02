72458dc54805f4a354258a840f6f2028e132cc7c
Tetiana Chykalova <chikalova.t@gmail.com>
2023-03-02 12:36:39 +0100

    added main style

---
+++ js/script.js
@@ -0,0 +1,38 @@
+new Swiper('.swiper-container-top',  {
+    navigation: {
+        prevEl: '.footer-arr-prev-top',
+        nextEl: '.footer-arr-next-top',
+    },
+
+    pagination: {
+        el: '.swiper-pagination-footer-top',
+        type: 'fraction',
+    },
+
+    simulateTouch: false,
+    loop: true,
+})
+
+new Swiper('.swiper-container-featured',  {
+    navigation: {
+        prevEl: '.footer-arr-prev-featured',
+        nextEl: '.footer-arr-next-featured',
+    },
+
+    pagination: {
+        el: '.swiper-pagination-footer-featured',
+        type: 'fraction',
+    },
+})
+
+new Swiper('.swiper-container-editor',  {
+    navigation: {
+        prevEl: '.footer-arr-prev-editor',
+        nextEl: '.footer-arr-next-editor',
+    },
+
+    pagination: {
+        el: '.swiper-pagination-footer-editor',
+        type: 'fraction',
+    },
+})
